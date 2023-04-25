ddfdfcbbc992675fcef5a886d1f01934fe76b4ed
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2023-04-25 18:27:33 +0200

    a

--- play_thing.js
+++ play_thing.js
@@ -1,8 +1,9 @@
 require('jquery-csv');
 const {MongoClient} = require('mongodb');
 async function main() {
-    uri='mongodb+srv://geldbogen:2KrudEufu@cluster0.en2gjsz.mongodb.net/?retryWrites=true&w=majority'
+    uri='mongodb+srv://geldbogen2:548L22XWv4iCxB8.en2gjsz.mongodb.net/?retryWrites=true&w=majority'
     const client = new MongoClient(uri);
-
+    client.connect();
+    db.createCollection("languages")
 }
 main();
